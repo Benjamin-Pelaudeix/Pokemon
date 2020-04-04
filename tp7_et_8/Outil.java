546a1e9fd36fffce9c6f568bfb8b621aebeca021
Benjamin PELAUDEIX <ben.pelau@hotmail.fr>
2020-04-04 22:23:21 +0200

    TP7 & 8 en cours - Exercice 5 terminé

---
+++ tp7_et_8/Outil.java
@@ -0,0 +1,26 @@
+package tp7_et_8;
+
+public class Outil extends Item implements ChangerItems {
+
+    public Outil(String nom, int frequence, int nombreUtilisations) {
+        super(nom, frequence, nombreUtilisations);
+    }
+
+    @Override
+    public void changer(Modifiable item) {
+        if (item != null && this.getUtilisationsRestantes() > 0) {
+            item.modifier();
+            this.utilisationsRestantes--;
+        }
+    }
+
+    @Override
+    public Item genererMemeItem(boolean generer) {
+       if (generer) {
+           return new Outil(this.getNom(), this.getFrequence(), this.getNombreUtilisations());
+       }
+       else {
+           return null;
+       }
+    }
+}
